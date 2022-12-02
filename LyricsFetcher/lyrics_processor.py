af5ec10916d295abb9a7e3341de34e41639cf993
Tayo Amuneke <tayo.amuneke@gmail.com>
2022-12-02 15:21:19 -0800

    Added script

--- LyricsFetcher/lyrics_processor.py
+++ LyricsFetcher/lyrics_processor.py
@@ -1,8 +1,20 @@
 import pickle
 from typing import  List
+import tabulate
 
-song_list: List[dict]
-with open('songs.pkl', 'rb') as f:
-    song_list = pickle.load(f)
 
-print(song_list)
+def get_data():
+    song_list: List[dict]
+    with open('LyricsFetcher/songs.pkl', 'rb') as f:
+        song_list = pickle.load(f)
+    return song_list
+
+def print_dataset(dataset):
+    header = dataset[0].keys()
+    rows =  [x.values() for x in dataset][:10]
+    print(tabulate.tabulate(rows, header))
+
+
+dataset = get_data()
+print(dataset[60])
+#print_dataset(dataset)
--- RocketBE/SongProcessor/flair_processor.py
+++ RocketBE/SongProcessor/flair_processor.py
@@ -1,4 +1,5 @@
 
+import random
 from typing import List
 from RocketBE.Model.request_model import Song
 from RocketBE.SongProcessor.processor import SongProcessor
@@ -11,22 +12,37 @@ class FlairSentimentAnalyzer(SongProcessor):
         self.process_data()
 
     def process_input(self, user_mood) -> List[Song] :
-        return [
-            Song(
-                artiste= "Ed Sheeran",
-                title= "A Team",
-                link = "some url",
-                thumbnail = "https://w7.pngwing.com/pngs/700/182/png-transparent-ed-sheeran-divide-musician-shape-of-you-others-miscellaneous-tshirt-microphone-thumbnail.png"
-            ),
-            Song(
-                artiste= "Selena Gomez",
-                title= "Rare",
-                link = "some url",
-                thumbnail = "https://integralatampost.s3.amazonaws.com/uploads/article/picture/20581/2020-01-16_16_092020-01-16_16_0920200118_Todo-sobre-el-nuevo-%C3%A1lbum-de-Selena-G%C3%B3mez_-Rare.jpg",
+        random_ten_items = random.sample(super().get_data(), 10)
+        result: List[Song] = []
+        for song in random_ten_items:
+            result.append(
+                    Song(
+                        artiste=   song["artiste"],
+                        title=   song["title"],
+                        link = "https://twitter.com/elon",
+                        thumbnail = song["thumbnail"]
+                    )
             )
-        ]
+              
+        return result   
+            
+        # return [
+        #     Song(
+        #         artiste= "Ed Sheeran",
+        #         title= "A Team",
+        #         link = "some url",
+        #         thumbnail = "https://w7.pngwing.com/pngs/700/182/png-transparent-ed-sheeran-divide-musician-shape-of-you-others-miscellaneous-tshirt-microphone-thumbnail.png"
+        #     ),
+        #     Song(
+        #         artiste= "Selena Gomez",
+        #         title= "Rare",
+        #         link = "some url",
+        #         thumbnail = "https://integralatampost.s3.amazonaws.com/uploads/article/picture/20581/2020-01-16_16_092020-01-16_16_0920200118_Todo-sobre-el-nuevo-%C3%A1lbum-de-Selena-G%C3%B3mez_-Rare.jpg",
+        #     )
+        # ]
         
 
     def process_data(self) -> None:
-         data: List[dict] = super().get_data()
-         print(data)
+        data: List[dict] = super().get_data()
+        random_ten_items = random.sample(data, 10)
+        
