3ae1bc321f5dcdc2323d87488346a84886217a74
musicalmaya12 <109922285+musicalmaya12@users.noreply.github.com>
2022-12-03 01:30:54 -0600

    Test code that creates dataframe with scores and labels

--- LyricsFetcher/lyrics_processor.py
+++ LyricsFetcher/lyrics_processor.py
@@ -1,18 +1,38 @@
 import pickle
-from typing import  List
-import tabulate
+from typing import List
+from flair.models import TextClassifier
+from flair.data import Sentence
 import pandas as pd
 
 
-def get_data()-> pd.DataFrame:
-    song_list: List[dict]
-    with open('LyricsFetcher/songs.pkl', 'rb') as f:
+def get_data():
+    song_list = List[dict]
+    with open('songs.pkl', 'rb') as f:
         song_list = pickle.load(f)
     df = pd.DataFrame.from_records(song_list)
+    process_data(df)
     return df
+
+def process_data(df):
+    scores = []
+    labels = []
+    textClassifier = TextClassifier.load('en-sentiment')
+    for lyric in df['lyrics']:
+        songLyrics = Sentence(lyric)
+        textClassifier.predict(songLyrics)
+        classified = songLyrics.labels[0]
+        scores.append(songLyrics.score)
+        if "POSITIVE" in str(classified):
+            labels.append("pos")
+        elif "NEGATIVE" in str(classified):
+            labels.append("neg")
+        else:
+            labels.append("neu")
+    final_df = pd.DataFrame({'score': scores, 'label': labels})
+    final_df = pd.concat([df, final_df], axis=1)
+    
+    print(final_df)
 
 
 
 dataset = get_data()
-print(dataset.head())
-#print_dataset(dataset)
