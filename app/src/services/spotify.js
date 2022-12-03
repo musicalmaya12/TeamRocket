728eef1001078c91b7f1b6f312d9e38007f541d5
jnwaogbe2 <109999483+jnwaogbe2@users.noreply.github.com>
2022-12-02 20:19:39 -0500

    Initial commit

--- app/src/services/spotify.js
+++ app/src/services/spotify.js
@@ -1,20 +1,82 @@
 import { Buffer } from 'buffer';
 
 const client_id = '5aef7021adaa4616a023abf0deaf6e9b'; // Your client id
-const client_secret = ''; // Your secret
+const client_secret = 'e60371614a224640b59c0ac21bd5a616'; // Your secret
 
 const SPOTIFY_URL = 'https://api.spotify.com/v1/search?';
 
-export const getTracks = (query) => {
-    fetch(SPOTIFY_URL + new URLSearchParams({
-        q: query,
-        type: 'track',
-        limit: 5
-    }), {
+var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+};
+
+export const getSpotifyAuth = () => {
+    let token = null;
+
+    fetch(authOptions.url, {
+        method: 'POST',
         headers: {
-            'Authorization': 'Bearer ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
-        }
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
     })
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log("Get here")
+                console.log(result);
+                token = result.access_token
+            },
+            (error) => {
+                console.log("Token error")
+                console.log(error);
+            }
+        )
+
+    // request.post(authOptions, function (error, response, body) {
+    //     if (!error && response.statusCode === 200) {
+    //         token = body.access_token;
+    //     }
+    // });
+
+    return token;
+}
+
+
+
+export const getTracks = (query) => {
+    fetch(authOptions.url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
+    })
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log("Get here")
+                console.log(result);
+                const token = result.access_token
+
+                return fetch(SPOTIFY_URL + new URLSearchParams({
+                    q: query,
+                    type: 'track',
+                    limit: 5
+                }), {
+                    headers: {
+                        'Authorization': 'Bearer ' + token
+                    }
+                });
+
+            }
+        )
         .then(res => res.json())
         .then(
             (result) => {
@@ -24,4 +86,9 @@ export const getTracks = (query) => {
                 console.log(error);
             }
         )
+        .catch(function (err) {
+            // Log any errors
+            console.log('something went wrong', err);
+
+        });
 }
